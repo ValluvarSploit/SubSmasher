471b0a8063b983e2dd22a8f3ef71ac57e74a2722
ValluvarSploit <alexandar.thangavel@gmail.com>
2021-03-01 11:05:52 +0530

    subsmaher born

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+if [[ $(id -u) != 0 ]]; then
+    echo -e "\n[!] Install.sh requires root privileges"
+    exit 0
+fi
+
+apt-get update
+apt-get install golang -y
+apt-get install gunzip -y
+apt-get install zip -y
+apt-get install git -y
+apt-get install python3-pip
+
+mkdir -p ./tools
+cd tools/
+git clone https://github.com/aboul3la/Sublist3r
+cd Sublist3r
+pip3 install -r requirements.txt
+cd ../
+
+git clone https://github.com/m8r0wn/subscraper
+cd subscraper
+python3 setup.py install
+cd ../
+
+mkdir -p assetfinder
+cd assetfinder
+wget https://github.com/tomnomnom/assetfinder/releases/download/v0.1.1/assetfinder-linux-amd64-0.1.1.tgz
+gunzip -c assetfinder-linux-amd64-0.1.1.tgz |tar xvf -
+chmod +x assetfinder
+cd ../
+
+mkdir -p subfinder 
+cd subfinder
+wget https://github.com/projectdiscovery/subfinder/releases/download/v2.4.5/subfinder_2.4.5_linux_amd64.tar.gz 
+tar -xzvf subfinder_2.4.5_linux_amd64.tar.gz
+chmod +x subfinder
+cd ../
+
+sudo snap install amass
+
+mkdir -p jq
+cd jq
+wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 
+mv jq-linux64 jq 
+chmod +x jq
+../
+
+mkdir -p findomain
+cd findomain
+wget https://github.com/Findomain/Findomain/releases/download/2.1.5/findomain-linux 
+mv findomain-linux findomain
+chmod +x findomain
+
---
+++ subsmasher.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+help() {
+   echo "Usage: subsmasher.sh -d example.com -o subdomains.txt"
+   echo "\t-d Domain Name\n"
+   echo "\t-o Output File Name\n"
+   exit 1
+}
+
+while getopts "d:o:" option
+do
+   case "$option" in
+      d ) TARGET="$OPTARG" ;;
+      o ) OUTPUT="$OPTARG" ;;
+      ? ) help ;;
+   esac
+done
+
+if [ -z "$TARGET" ] || [ -z "$OUTPUT" ]
+then
+   echo "Target Domain or Output filename is not provided."
+   help
+fi
+
+OUT_DIR=$(pwd)
+TOOLS_DIR=$(pwd)/tools
+
+echo [*] Running Subsmasher : ${TARGET}
+
+cd $TOOLS_DIR/subscraper
+echo "[*] Launching SubScraper"
+python3 subscraper.py $TARGET -o $OUT_DIR/subscraper.txt &> /dev/null &
+
+cd $TOOLS_DIR/Sublist3r
+echo "[*] Launching Sublist3r"
+python3 sublist3r.py -d $TARGET -o $OUT_DIR/sublist3r.txt &> /dev/null &
+
+cd $TOOLS_DIR/assetfinder
+echo "[*] Launching Assetfinder"
+./assetfinder --subs-only $TARGET > $OUT_DIR/assetfinder.txt &
+
+cd $TOOLS_DIR/subfinder
+echo "[*] Launching Subfinder"
+./subfinder -d $TARGET -all --silent > $OUT_DIR/subfinder.txt & 
+
+cd $TOOLS_DIR/findomain
+echo "[*] Launching Findomain"
+./findomain -t $TARGET -q > $OUT_DIR/findomain.txt &
+
+echo "[*] Launching Amass"
+amass enum -passive -d $TARGET > $OUT_DIR/amass.txt &
+
+echo "[*] Launching Certspotter"
+curl -s "https://api.certspotter.com/v1/issuances?domain=$TARGET&expand=dns_names" | ./$TOOLS_DIR/jq/jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | grep $TARGET > $OUT_DIR/certspotter.txt &
+
+echo "[*] Launching Crt.sh"
+curl -s "https://crt.sh/?q=%.$TARGET" | grep $TARGET | cut -d '>' -f2 | cut -d '<' -f1 | grep -v " " > $OUT_DIR/crtsh.txt &
+
+echo "[*] Waiting until all scripts complete..."
+wait
+
+cd $OUT_DIR
+(cat subscraper.txt sublist3r.txt assetfinder.txt subfinder.txt findomain.txt amass.txt certspotter.txt crtsh.txt | sort -u) > $OUTPUT
+rm subscraper.txt sublist3r.txt assetfinder.txt subfinder.txt findomain.txt amass.txt certspotter.txt crtsh.txt
+
+RES=$(cat $OUTPUT | wc -l)
+echo -e "\n[+] SubWalker complete with ${RES} results"
+echo "[+] Output saved to: $OUT_DIR/$OUTPUT"
+exit 0
